333548df094646a22475977c4137ca239c0dfc1e
Aritra <aritradas@gmail.com>
2020-05-08 20:50:26 +0530

    python code for stock v0.1

--- Stock_Data_Analysis/Bar_chart_plotting.py
+++ Stock_Data_Analysis/Bar_chart_plotting.py
@@ -1,12 +1,44 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 from sqlalchemy import create_engine
-import datetime
 import matplotlib.pyplot as plt
-import openpyxl
-from openpyxl import Workbook
-from openpyxl.drawing.image import Image
+
+class BarChartPlot():
+    def __init__(self, table_name , dbname, host, user, pwd, port  ):
+        self.table_name = table_name
+        self.dbname = dbname
+        self.host = host
+        self.user= user
+        self.pwd = pwd
+        self.port = port
+
+    def db_fetch_query(self, my_query):
+        conn = f'postgres://{user}:{pwd}@{host}:{port}/{dbname}'
+        engine = create_engine(conn)
+        df = pd.read_sql_query(my_query, conn)
+        return df
+    def bar_chart_plot(self,df):
+        k1 = df['price_change'].to_list()
+        df['dummy_col'] = df['price_change'] > 0
+        k2 = df['symbol'].to_list()
+        k1= df.dummy_col.map( {True: 'g'})
+
+        ax = df['price_change'].plot(kind='bar',
+                             color=df.dummy_col.map({True: 'g', False: 'r'}))
+        ax.set_title("Sector performance analysis")
+
+        rects = ax.patches
+        plt.minorticks_on()
+        plt.grid(which='major', linestyle='-', linewidth='0.5', color='red')
+        # Customize the minor grid
+        plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+        ax.set_ylabel("PRICE", fontsize=12)
+        ax.set_xticklabels(k2, rotation=1)
+        plt.show()
+
+
+
+
+
 
 
 table_name = 'historical_data_indices'
@@ -16,64 +48,44 @@ user = 'postgres'
 pwd = 'password'
 port = '5432'
 
-conn = f'postgres://{user}:{pwd}@{host}:{port}/{dbname}'
-print(conn)
-engine = create_engine(conn)
-
-#for i in
-
-mysql = '''SELECT trim(M1.SYMBOL) SYMBOL, M1.OPEN_PRICE  , M2.CLOSE_PRICE , (M2.CLOSE_PRICE-M1.OPEN_PRICE) PRICE_CHANGE
+#6month plotting
+mysql_6 = '''SELECT trim(M1.SYMBOL) SYMBOL, M1.OPEN_PRICE  , M2.CLOSE_PRICE , (M2.CLOSE_PRICE-M1.OPEN_PRICE) PRICE_CHANGE
 FROM HISTORICAL_DATA_INDICES M1 , HISTORICAL_DATA_INDICES M2
 WHERE M1.DATE_RECORDED = '2019-11-29'
 AND M2.DATE_RECORDED = '2020-05-07'
 AND M1.SYMBOL = M2.SYMBOL'''
 
-df = pd.read_sql_query(mysql, conn)
+#3month plotting
+mysql_3 = '''SELECT trim(M1.SYMBOL) SYMBOL, M1.OPEN_PRICE  , M2.CLOSE_PRICE , (M2.CLOSE_PRICE-M1.OPEN_PRICE) PRICE_CHANGE
+FROM HISTORICAL_DATA_INDICES M1 , HISTORICAL_DATA_INDICES M2
+WHERE M1.DATE_RECORDED = '2020-02-01'
+AND M2.DATE_RECORDED = '2020-05-07'
+AND M1.SYMBOL = M2.SYMBOL'''
 
-#print(df)
-print(df['price_change'].to_list())
-k1 = df['price_change'].to_list()
-df['dummy_col'] = df['price_change'] > 0
-#print(k1)
-k2 = df['symbol'].to_list()
-print(k2)
-k1= df.dummy_col.map( {True: 'g'})
+mysql_31 = '''SELECT trim(M1.SYMBOL) SYMBOL, M1.OPEN_PRICE  , M2.CLOSE_PRICE , (M2.CLOSE_PRICE-M1.OPEN_PRICE) PRICE_CHANGE
+FROM HISTORICAL_DATA_INDICES M1 , HISTORICAL_DATA_INDICES M2
+WHERE M1.DATE_RECORDED = '2019-11-29'
+AND M2.DATE_RECORDED = '2020-02-01'
+AND M1.SYMBOL = M2.SYMBOL'''
 
-ax = df['price_change'].plot(kind='bar',
-                             color=df.dummy_col.map({True: 'g', False: 'r'}))
-
-ax.set_title("Sector performance analysis for 6 months old data")
-
-rects = ax.patches
-plt.minorticks_on()
-plt.grid(which='major', linestyle='-', linewidth='0.5', color='red')
-# Customize the minor grid
-plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+#one month old data
+mysql_1 = '''SELECT trim(M1.SYMBOL) SYMBOL, M1.OPEN_PRICE  , M2.CLOSE_PRICE , (M2.CLOSE_PRICE-M1.OPEN_PRICE) PRICE_CHANGE
+FROM HISTORICAL_DATA_INDICES M1 , HISTORICAL_DATA_INDICES M2
+WHERE M1.DATE_RECORDED = '2020-04-01'
+AND M2.DATE_RECORDED = '2020-05-06'
+AND M1.SYMBOL = M2.SYMBOL'''
 
 
-
-#ax = df[['price_change']].plot(kind = 'barh',
-#color = df.dummy_col.map({True : 'g', False: 'r'}) )
-for lp in k2:
-    m1 = lp
-    print(m1)
-#ax.set_xlabel(k2, fontsize=12)
-#ax.xaxis_date()     # interpret the x-axis values as dates
-#fig.autofmt_xdate() # make space for and rotate the x-axis tick labels
-
-ax.set_ylabel("PRICE", fontsize=12)
-label1 = (label12 for label12 in k2)
-
-print(label1)
-#label = "{:.1f}".format(label1)
-#print(label)
-
-ax.set_xticklabels(k2, rotation=1)
-plt.show()
+#1 week old data
+mysql_1w = '''SELECT trim(M1.SYMBOL) SYMBOL, M1.OPEN_PRICE  , M2.CLOSE_PRICE , (M2.CLOSE_PRICE-M1.OPEN_PRICE) PRICE_CHANGE
+FROM HISTORICAL_DATA_INDICES M1 , HISTORICAL_DATA_INDICES M2
+WHERE M1.DATE_RECORDED = '2020-04-26'
+AND M2.DATE_RECORDED = '2020-05-06'
+AND M1.SYMBOL = M2.SYMBOL'''
 
 
-
-
-
+obj1 = BarChartPlot(table_name, dbname, host, user, pwd, port)
+df = obj1.db_fetch_query(mysql_1)
+obj1.bar_chart_plot(df)
 
 
